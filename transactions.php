c4510d22e4233a531375d6b3c97a118b6cffc141
Roaa-pixel <72927350+Roaa-pixel@users.noreply.github.com>
2022-03-19 03:45:06 +0200

    Update transactions.php

--- transactions.php
+++ transactions.php
@@ -177,39 +177,7 @@
                         </div>
 
                     </div>
-                    <div class="col-md-4 sidebar">
-                        <div class="sidebar-box">
-                            <form action="#" class="search-form">
-                                <div class="form-group">
-                                    <span class="icon fa fa-search"></span>
-                                    <input type="text" class="form-control" placeholder="Type a keyword and hit enter">
-                                </div>
-                            </form>
-                        </div>
-                        <div class="sidebar-box">
-                            <div class="categories">
-                                <h3>Categories</h3>
-                                <li><a href="#">Creatives <span>(12)</span></a></li>
-                                <li><a href="#">News <span>(22)</span></a></li>
-                                <li><a href="#">Design <span>(37)</span></a></li>
-                                <li><a href="#">HTML <span>(42)</span></a></li>
-                                <li><a href="#">Web Development <span>(14)</span></a></li>
-                            </div>
-                        </div>
-                        <div class="sidebar-box">
-                            <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid mb-4">
-                            <h3>About The Author</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam vero culpa sapiente consectetur similique, inventore eos fugit cupiditate
-                                numquam!</p>
-                            <p><a href="#" class="btn btn-primary btn-sm">Read More</a></p>
-                        </div>
-
-                        <div class="sidebar-box">
-                            <h3>Paragraph</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam vero culpa sapiente consectetur similique, inventore eos fugit cupiditate
-                                numquam!</p>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         </section>
@@ -217,45 +185,7 @@
 
         <footer class="site-footer">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="row">
-                            <div class="col-md-5">
-                                <h2 class="footer-heading mb-4">About Us</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
-                            </div>
-                            <div class="col-md-3 ml-auto">
-                                <h2 class="footer-heading mb-4">Quick Links</h2>
-                                <ul class="list-unstyled">
-                                    <li><a href="#about-section" class="smoothscroll">Terms</a></li>
-                                    <li><a href="#about-section" class="smoothscroll">Policy</a></li>
-                                    <li><a href="#about-section" class="smoothscroll">About Us</a></li>
-                                    <li><a href="#services-section" class="smoothscroll">Services</a></li>
-                                    <li><a href="#testimonials-section" class="smoothscroll">Testimonials</a></li>
-                                    <li><a href="#contact-section" class="smoothscroll">Contact Us</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-md-3 footer-social">
-                                <h2 class="footer-heading mb-4">Follow Us</h2>
-                                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-                                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-                                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
-                        <form action="#" method="post" class="footer-subscribe">
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary text-black" type="button" id="button-addon2">Send</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+               
                 <div class="row pt-5 mt-5 text-center">
                     <div class="col-md-12">
                         <div class="border-top pt-5">
